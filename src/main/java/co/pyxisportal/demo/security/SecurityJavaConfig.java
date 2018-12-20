9523d7c5b99b3e4a047e57e5ea19170b6c2cf08c
Rafael López <ralphdoe12@gmail.com>
2018-12-20 09:31:02 -0500

    Security

--- src/main/java/co/pyxisportal/demo/security/SecurityJavaConfig.java
+++ src/main/java/co/pyxisportal/demo/security/SecurityJavaConfig.java
@@ -1,4 +1,40 @@
 package co.pyxisportal.demo.security;
 
-public class SecurityJavaConfig {
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+	@Resource
+	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN").and()
+				.withUser("user").password(encoder().encode("userPass")).roles("USER");
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.csrf().disable().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+				.authorizeRequests().antMatchers("/demos").authenticated().antMatchers("/api/admin/**").hasRole("ADMIN")
+				.and().formLogin().successHandler(new SimpleUrlAuthenticationSuccessHandler())
+				.failureHandler(new SimpleUrlAuthenticationFailureHandler()).and().logout();
+	}
 }
